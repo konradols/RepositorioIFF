daaa8e16d2e00ef09f0270fd378c67d9b9a1ea84
Anesi <daniel.o.anesi@gmail.com>
2019-11-25 10:34:20 -0300

    listagem

--- Tela/listagemUsuario.php
+++ Tela/listagemUsuario.php
@@ -1,130 +1,144 @@
 <!DOCTYPE html>
 <?php
-if (!isset($_SESSION)) {
-    session_start();
-}
-include_once '../Base/header.php';
-include_once '../Base/nav.php';
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+    include_once '../Base/header.php';
+    include_once '../Base/nav.php';
+    include_once '../Controle/usuarioPDO.php';
+    include_once '../Modelo/Usuario.php';
+    $usuarioPDO = new UsuarioPDO();
+    $stmtUsuarios = $usuarioPDO->selectUsuario();
+    $usuarios = $stmtUsuarios->fetchAll();
 ?>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Protótipo Home Page</title>
-        <style type="text/css">
-            table{border: none;}
-            table tr td{border: none;}
-        </style>
-    </head>
-    <body>
-        <div class="row">
-            <?php
-            include_once '../Modelo/Usuario.php';
-            if (isset($_SESSION['usuario'])) {
-                $logado = new usuario(unserialize($_SESSION['usuario']));
-            }
-            ?>
-
+<body>
+<?php
+    include_once '../Modelo/Usuario.php';
+    if (isset($_SESSION['usuario'])) {
+        $logado = new usuario(unserialize($_SESSION['usuario']));
+    }
+?>
+<main>
+    <div class="row center">
+        <div class="card z-depth-3 col s12 m12 l10 offset-l1">
             <div class="row">
-                <div class="col l10 card offset-l1">
-                    <h5 class="center">Lista de Usuários</h5>
-                    <!--                    <div class="col l12" style="margin-left: 15px;">
-                                            <table>
-                                                <tr>
-                                                    <td>
-                                                        <label for="categoria">Filtrar por</label>
-                                                        <div class="input-field col s12 center" style="margin-top: -4px;">
-                                                            <select name="categoria">
-                                                                <option value="id">Id</option>
-                                                                <option value="nome">Nome</option>
-                                                                <option value="email">E-mail</option>
-                                                                <option value="usuario">Usuário</option>
-                                                                <option value="categoria">Categoria</option>
-                                                                <option value="administrador">Administradores</option>
-                                                            </select>
-                                                        </div>
-                                                    </td>
-                                                    <td>
-                                                        <input type="text" name="pesquisa" placeholder="Pesquise">
-                                                    </td>
-                                                    <td>
-                                                        <input type="submit" id="btn-pesquisar" class="btn corpadrao inline" value="Pesquisar">
-                                                    </td>
-                                                </tr>
-                                            </table>
-                                        </div>-->
-
-                    <div id="tabela" class="loader">
-
-                        <table class="striped">
-                            <tr>
-                                <td>Id</td>
-                                <td>Nome</td>
-                                <td>Email</td>
-                                <td>Usuário</td>
-                                <td>Categoria</td>
-                                <td>Administrador</td>
-                            </tr>
-                            <?php
-                            include_once '../Controle/usuarioPDO.php';
-                            include_once '../Modelo/usuario.php';
-                            $usuarioListar = new usuarioPDO();
-                            if (isset($_POST['pesquisar'])) {
-                                $pesquisa = $_POST['pesquisar'];
-                                $metodo = $_POST['select'];
-                                $sql = $usuarioListar->$metodo($pesquisa);
-                            } else {
-                                $sql = $usuarioListar->selectUsuario();
-                            }
-                            if ($sql != false) {
-
-                                while ($resultado = $sql->fetch()) {
-                                    $us = new usuario($resultado);
-                                    echo "<tr>";
-                                    echo "<td>" . $us->getId() . "</td>";
-                                    echo "<td>" . $us->getNome() . "</td>";
-                                    echo "<td>" . $us->getEmail() . "</td>";
-                                    echo "<td>" . $us->getUsuario() . "</td>";
-                                    echo "<td>" . $us->getCategoria() . "</td>";
-                                    echo "<td>" . $us->getAdministrador() . "</td>";
-
-//                        -----------------------------------------------------------
-                                    if (($us->getAdministrador() == 'true')) {
-                                        echo "<td>";
-                                        ?><input type="button" class="btn corpadrao ativoInativo" caminho="../Controle/usuarioControle.php?function=tornarUsuarioNaoAdm&id=
-                                               <?php echo $us->getId(); ?>" value="Ativo">
-                                               <?php
-                                               echo "</td>";
-                                           } else {
-                                               echo "<td>";
-                                               ?>
-                                        <input type="button" class="btn red darken-2 ativoInativo" caminho="../Controle/usuarioControle.php?function=tornarUsuarioAdm&id=
-                                               <?php echo $us->getId(); ?>" value="Inativo"><?php
-                                               echo "</td>";
-                                           }
-//                        -----------------------------------------------------------
-
-
-                                           echo "<td>";
-                                           ?><a class="btn corpadrao" href="./verMais.php?id=<?php echo $us->getId(); ?>">Ver mais</a><?php
-                                    echo "</td>";
-                                    echo "</tr>";
-                                }
-                            } else {
-                                echo "<tr><td><h6>Nenhum resultado econtrado</h6></td></tr>";
-                            }
-                            ?>
-                        </table>
-                    </div>
-
-                    <br/><br/><br/><br/>
-
-                </div>
+                <h5 class="center">Listagem Usuários</h5>
             </div>
-
+            <div class="row">
+                <?php
+                    if ($usuarios) { ?>
+                        <table class="col s10 offset-s1 hide-on-small-only hide-on-med-only">
+                            <thead>
+                            <tr>
+                                <th class="center">Foto</th>
+                                <th class="center">Nome</th>
+                                <th class="center">Email</th>
+                                <th class="center">Categoria</th>
+                                <th class="center">Administrador</th>
+                                <th class="center">Perfil</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                                foreach ($usuarios as $linha) {
+                                    $usuario = new usuario($linha);
+                                    ?>
+                                    <tr>
+                                        <td><img src="../<?php echo $usuario->getFoto(); ?>" width="80" height="80">
+                                        </td>
+                                        <td class="center"><?php echo $usuario->getNome(); ?></td>
+                                        <td class="center"><?php echo $usuario->getEmail(); ?></td>
+                                        <td class="center"><?php echo $usuario->getCategoria(); ?></td>
+                                        <td class="center">
+                                            <?php
+                                                if($usuario->getAdministrador() == 1) {
+                                                    ?>
+                                                    <a href="../Controle/usuarioControle.php?function=removeAdm&id_usuario=<?php echo $usuario->getIdUsuario() ?>" class="btn red darken-3">Remover</a>
+                                                    <?php
+                                                } else {
+                                                    ?>
+                                                    <a href="../Controle/usuarioControle.php?function=addAdm&id_usuario=<?php echo $usuario->getIdUsuario() ?>" class="btn green darken-2">Adicionar</a>
+                                                    <?php
+                                                }
+                                            ?>
+                                        </td>
+                                        <td class="center">
+                                            <a href="./perfil.php?id_usuario=<?php echo $usuario->getIdUsuario(); ?>"
+                                               class="btn waves-effect">Ver perfil</a></td>
+                                    </tr>
+                                    <?php
+                                } ?>
+                            </tbody>
+                        </table>
+                        <?php
+                    } else {
+                        echo "<h5 class='hide-on-small-and-down'>Nenhum cadastro pendente!</h5>";
+                    }
+                ?>
+            </div>
+            <div class="row">
+                <?php if ($usuarios) { ?>
+                    <ul class="collapsible hide-on-large-only">
+                        <?php
+                            foreach ($usuarios as $linha) {
+                                $usuario = new usuario($linha);
+                                ?>
+                                <li>
+                                    <div class="collapsible-header"><b><?php echo $usuario->getNome() ?></b></div>
+                                    <div class="collapsible-body">
+                                        <p class="center">
+                                            <img src="../<?php echo $usuario->getFoto() ?>" width="80" height="80">
+                                        </p>
+                                        <p class="left-align">
+                                            <b>Nome:</b> <?php echo $usuario->getNome() ?>
+                                        </p>
+                                        <p class="left-align">
+                                            <b>Email:</b> <?php echo $usuario->getEmail() ?>
+                                        </p>
+                                        <p class="left-align">
+                                            <b>Categoria:</b> <?php echo $usuario->getCategoria() ?>
+                                        </p>
+                                        <p>
+                                            <?php
+                                                if($usuario->getAdministrador() == 1) {
+                                                    ?>
+                                                    <a href="../Controle/usuarioControle.php?function=removeAdm&id_usuario=<?php echo $usuario->getIdUsuario() ?>" class="btn red darken-3">Remover Adiministrador</a>
+                                                    <?php
+                                                } else {
+                                                    ?>
+                                                    <a href="../Controle/usuarioControle.php?function=addAdm&id_usuario=<?php echo $usuario->getIdUsuario() ?>" class="btn green darken-2">Tornar Adiministrador</a>
+                                                    <?php
+                                                }
+                                            ?>
+                                        </p>
+                                        <p>
+                                            <a href="./perfil.php?id_usuario=<?php echo $usuario->getIdUsuario(); ?>"
+                                               class="btn waves-effect">Ver perfil</a></td>
+                                        </p>
+                                    </div>
+                                </li>
+                                <?php
+                            }
+                        ?>
+                    </ul>
+                    <?php
+                } else {
+                    echo "<h5 class='hide-on-large-only'>Nenhum cadastro pendente!</h5>";
+                }
+                ?>
+            </div>
+            <div class="row">
+                <a href="../index.php" class="btn waves-effect">Voltar</a>
+            </div>
         </div>
-
-        <?php
-        include_once '../Base/footer.php';
-        ?>
-    </body>
+    </div>
+</main>
+<script>
+    $('.collapsible').collapsible();
+</script>
+<?php
+    include_once '../Base/footer.php';
+?>
+</body>
 </html>
